48f7dbccdda9ffcd17624269e9553c2840318a07
madalinaionescu1 <i.madalina121@gmail.com>
2022-02-25 00:22:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './solidLogoWhite.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+	  <div>
+	    <h1>COMING SOON</h1>
+	  </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
